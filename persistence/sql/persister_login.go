dcc1aebdf23836de5878bbb71bcc186d12f3fdc8
Anirudh Oppiliappan <x@icyphox.sh>
2022-05-16 16:55:16 +0530

    feat(tracing): trace persistence operations

--- persistence/sql/persister_login.go
+++ persistence/sql/persister_login.go
@@ -17,12 +17,18 @@ import (
 var _ login.FlowPersister = new(Persister)
 
 func (p *Persister) CreateLoginFlow(ctx context.Context, r *login.Flow) error {
+	ctx, span := p.r.Tracer(ctx).Tracer().Start(ctx, "persistence.sql.CreateLoginFlow")
+	defer span.End()
+
 	r.NID = corp.ContextualizeNID(ctx, p.nid)
 	r.EnsureInternalContext()
 	return p.GetConnection(ctx).Create(r)
 }
 
 func (p *Persister) UpdateLoginFlow(ctx context.Context, r *login.Flow) error {
+	ctx, span := p.r.Tracer(ctx).Tracer().Start(ctx, "persistence.sql.UpdateLoginFlow")
+	defer span.End()
+
 	r.EnsureInternalContext()
 	cp := *r
 	cp.NID = corp.ContextualizeNID(ctx, p.nid)
@@ -30,6 +36,8 @@ func (p *Persister) UpdateLoginFlow(ctx context.Context, r *login.Flow) error {
 }
 
 func (p *Persister) GetLoginFlow(ctx context.Context, id uuid.UUID) (*login.Flow, error) {
+	ctx, span := p.r.Tracer(ctx).Tracer().Start(ctx, "persistence.sql.GetLoginFlow")
+	defer span.End()
 	conn := p.GetConnection(ctx)
 
 	var r login.Flow
@@ -42,6 +50,9 @@ func (p *Persister) GetLoginFlow(ctx context.Context, id uuid.UUID) (*login.Flow
 
 func (p *Persister) ForceLoginFlow(ctx context.Context, id uuid.UUID) error {
 	return p.Transaction(ctx, func(ctx context.Context, tx *pop.Connection) error {
+		ctx, span := p.r.Tracer(ctx).Tracer().Start(ctx, "persistence.sql.ForceLoginFlow")
+		defer span.End()
+
 		lr, err := p.GetLoginFlow(ctx, id)
 		if err != nil {
 			return err
